9da1a5907d777e78a05b1432ab84f5a163c498cb
Nathan Smith <nathansforyou@gmail.com>
2018-12-30 14:28:12 -0500

    Added routes and route params

--- node/FirstExpressApp/app.js
+++ node/FirstExpressApp/app.js
@@ -14,6 +14,19 @@ app.get("/cat", function(req, res){
   res.send("Meow");
 });
 
+app.get("/r/:subredditName", function(req, res){
+  var subreddit = req.params.subredditName;
+  res.send("Welcome to the " + subreddit.toUpperCase() + " subreddit");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res){
+  res.send("Welcome to the comments page");
+});
+
+app.get("*", function(req, res){
+  res.send("You are a star!");
+});
+
 // Tell Exprss to listen for requests (start server)
 app.listen(3000, function(){
   console.log("Server has started");
