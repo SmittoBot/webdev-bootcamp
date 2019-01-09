19cac593665a67b83aef0753995fd4e4288e502a
Nathan Smith <nathansforyou@gmail.com>
2019-01-09 00:09:39 -0500

    JSON request demo

--- node/APIs/first_request.js
+++ node/APIs/first_request.js
@@ -1,13 +1,8 @@
-var request = require('request');
-request('http://www.google.com', function(error, response, body){
-  if(error){
-    console.log("Rippy");
-    console.log(error);
-  } else {
-    if(response.statusCode == 200){
-      console.log(body);
-    } else {
-      console.log("Got another status code");
-    }
+const request = require('request');
+request('https://jsonplaceholder.typicode.com/users/1', function(error, response, body){
+  // eval(require('locus'))
+  if(!error && response.statusCode == 200){
+    const parsedData = JSON.parse(body);
+    console.log(`${parsedData.name} lives in ${parsedData.address.city}`);
   }
 });
