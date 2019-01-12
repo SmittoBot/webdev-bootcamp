283111765301b8644b3176eb850ff26bc442b29f
Nathan Smith <nathansforyou@gmail.com>
2019-01-11 23:56:51 -0500

    Movie app pages

--- node/APIs/movie_search_app/app.js
+++ node/APIs/movie_search_app/app.js
@@ -1,12 +1,19 @@
 var express = require("express");
 var request = require("request");
 var app = express();
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("search");
+});
 
 app.get("/results", function(req, res){
-  request("http://omdbapi.com/?s=miyazaki&apikey=thewdb", function(error, response, body){
+  var query = req.query.search;
+  var url = "http://omdbapi.com/?s=" + query + "&apikey=thewdb";
+  request(url, function(error, response, body){
     if(!error && response.statusCode == 200) {
-      var results = JSON.parse(body);
-      res.send(results["Search"][0]["Title"]);
+      var data = JSON.parse(body);
+      res.render("results", {data: data});
     }
   });
 });
