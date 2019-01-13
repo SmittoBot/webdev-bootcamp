25bf31c913bfbd17e0524708efb8f63e72a8f4a3
Nathan Smith <nathansforyou@gmail.com>
2019-01-13 15:48:58 -0500

    Module files

--- associations/references.js
+++ associations/references.js
@@ -1,64 +1,25 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/blog_demo", {useNewUrlParser: true});
 
-// POST - title, content
-var postSchema = new mongoose.Schema({
-  title: String,
-  content: String
-});
+var Post = require("./models/post");
+var User = require("./models/user");
 
-var Post = mongoose.model("Post", postSchema);
-
-// USER - email, name
-var userSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-  posts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Post"
+Post.create({
+  title: "How to do anything pt. 4",
+  content: "Acquire EVEN MORE swag"
+}, function(err, post){
+  User.findOne({email: "big@boi.swag"}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else {
+      foundUser.posts.push(post);
+      foundUser.save(function(err, data){
+        if(err){
+          console.log(err);
+        } else {
+          console.log(data);
+        }
+      });
     }
-  ]
-});
-
-var User = mongoose.model("User", userSchema);
-
-// User.create({
-//   email: "big@boi.swag",
-//   name: "Tonyyy"
-// });
-
-// Post.create({
-//   title: "How to do anything",
-//   content: "Acquire swag"
-// }, function(err, post){
-//   console.log(post);
-// });
-
-// Post.create({
-//   title: "How to do anything pt. 3",
-//   content: "Acquire MORE swag"
-// }, function(err, post){
-//   User.findOne({email: "big@boi.swag"}, function(err, foundUser){
-//     if(err){
-//       console.log(err);
-//     } else {
-//       foundUser.posts.push(post);
-//       foundUser.save(function(err, data){
-//         if(err){
-//           console.log(err);
-//         } else {
-//           console.log(data);
-//         }
-//       });
-//     }
-//   });
-// });
-
-User.findOne({email: "big@boi.swag"}).populate("posts").exec(function(err, user){
-  if(err){
-    console.log(err);
-  } else {
-    console.log(user);
-  }
+  });
 });
