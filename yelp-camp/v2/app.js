3d912f9e701d7805de66e1ce870c22f44a39b705
Nathan Smith <nathansforyou@gmail.com>
2019-01-12 19:38:25 -0500

    Started v2 of yelpcamp

---
+++ yelp-camp/v2/app.js
@@ -0,0 +1,69 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var app = express();
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Salmon Creek",
+//     image: "https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg"
+//   }, function(err, campground){
+//     if(err){
+//       console.log(err);
+//     } else {
+//       console.log("Newly created campground: " + campground);
+//     }
+//   }
+// );
+
+var campgrounds = [
+  {name: "Salmon Creek", image: "https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg"},
+  {name: "Granite Hill", image: "https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg"}
+];
+
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds", {campgrounds: allCampgrounds});
+    }
+  });
+});
+
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  Campground.create(newCampground, function(err, newlyCreated){
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new.ejs");
+});
+
+app.listen(3000, function(){
+  console.log("YelpCamp has started")
+});
