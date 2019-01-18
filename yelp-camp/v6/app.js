f2040e73e1f746d71d6eb0bd2de37bce4290e6f3
Nathan Smith <nathansforyou@gmail.com>
2019-01-17 23:48:06 -0500

    Completed authentication unit

--- yelp-camp/v6/app.js
+++ yelp-camp/v6/app.js
@@ -27,6 +27,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get("/", function(req, res){
   res.render("landing");
 });
@@ -75,7 +80,7 @@ app.get("/campgrounds/:id", function(req, res){
 });
 
 // COMMENTS ROUTES
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, campground){
     if(err){
       console.log(err);
@@ -85,7 +90,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
   });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, campground){
     if(err){
       console.log(err);
@@ -111,8 +116,39 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
   var newUser = new User({username: req.body.username});
-  User.register(newUser, req.body.password);
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds");
+    });
+  });
 });
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }), function(req, res){
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+};
 
 app.listen(3000, function(){
   console.log("YelpCamp has started")
